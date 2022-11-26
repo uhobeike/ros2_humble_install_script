4cb505d6010facd3adcc6ab20a9a2fa2ce0b2900
Tatsuhiro Ikebe <beike315@icloud.com>
2022-11-27 01:15:10 +0900

    Feature ros2 humble install scripts (#1)

---
+++ install_desktop.sh
@@ -0,0 +1,19 @@
+#/bin/bash -xve
+
+sudo apt update && sudo apt install curl
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+sudo apt update -y
+sudo apt upgrade -y
+
+sudo apt install ros-humble-desktop
+sudo apt -y install gazebo
+sudo apt install ros-humble-gazebo-*
+
+grep "source /opt/ros/humble/setup.bash" ~/.bashrc
+if [ $? = 0 ]; then
+  :
+else
+  echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+fi
