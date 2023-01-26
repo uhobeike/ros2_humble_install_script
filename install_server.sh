f0267745c0b25955732cdc39f598487a665333c7
uhobeike <beike315@icloud.com>
2023-01-26 19:50:49 +0900

    Feature install server script

---
+++ install_server.sh
@@ -0,0 +1,18 @@
+#/bin/bash -xve
+
+sudo apt update && sudo apt install curl
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+sudo apt update -y
+sudo apt upgrade -y
+
+sudo apt install ros-humble-ros-base
+sudo apt install python3-colcon-common-extensions
+
+grep "source /opt/ros/humble/setup.bash" ~/.bashrc
+if [ $? = 0 ]; then
+  :
+else
+  echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+fi
