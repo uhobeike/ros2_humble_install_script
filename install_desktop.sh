c5994986b43f611624e05357c0e34ab55fa9eef6
Tatsuhiro Ikebe <beike315@icloud.com>
2023-05-29 20:55:46 +0900

    Feature python3-rosdep2 install (#5)

--- install_desktop.sh
+++ install_desktop.sh
@@ -7,9 +7,9 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-a
 sudo apt update -y
 sudo apt upgrade -y
 
-sudo apt install ros-humble-desktop python3-colcon-common-extensions
+sudo apt install -y ros-humble-desktop python3-colcon-common-extensions python3-rosdep2
 sudo apt -y install gazebo
-sudo apt install ros-humble-gazebo-*
+sudo apt install -y ros-humble-gazebo-*
 
 grep "source /opt/ros/humble/setup.bash" ~/.bashrc
 if [ $? = 0 ]; then
--- install_server.sh
+++ install_server.sh
@@ -7,8 +7,7 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-a
 sudo apt update -y
 sudo apt upgrade -y
 
-sudo apt install ros-humble-ros-base python3-colcon-common-extensions
-sudo apt install python3-colcon-common-extensions
+sudo apt install -y ros-humble-ros-base python3-colcon-common-extensions python3-rosdep2
 
 grep "source /opt/ros/humble/setup.bash" ~/.bashrc
 if [ $? = 0 ]; then
