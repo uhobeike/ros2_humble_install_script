715b1c0e149252091c50cd09596e485ab32a0fa7
Tatsuhiro Ikebe <beike315@icloud.com>
2023-06-04 13:16:46 +0900

    Feature setup colcon tab completion and setup colcon_cd (#8)

--- install_server.sh
+++ install_server.sh
@@ -9,6 +9,8 @@ sudo apt upgrade -y
 
 sudo apt install -y ros-humble-ros-base python3-colcon-common-extensions python3-rosdep2
 
+echo -e "\n## Execute $0 $(date +'%Y/%m/%d %H:%M:%S')"  >> ~/.bashrc
+
 grep "source /opt/ros/humble/setup.bash" ~/.bashrc
 if [ $? = 0 ]; then
   :
@@ -21,4 +23,19 @@ if [ $? = 0 ]; then
   :
 else
   echo "export ROS_DOMAIN_ID=1" >> ~/.bashrc
+fi
+
+grep "source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash" ~/.bashrc
+if [ $? = 0 ]; then
+  :
+else
+  echo "source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash" >> ~/.bashrc
+fi
+
+grep "export _colcon_cd_root=" ~/.bashrc
+if [ $? = 0 ]; then
+  :
+else
+  echo "source /usr/share/colcon_cd/function/colcon_cd.sh" >> ~/.bashrc
+  echo "export _colcon_cd_root=~" >> ~/.bashrc
 fi
