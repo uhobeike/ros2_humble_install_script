646faeb18a3e2dcc4e28fae7b1aef30877c64265
Tatsuhiro Ikebe <beike315@icloud.com>
2023-06-04 13:15:16 +0900

    Feature setup colcon tab completion and setup colcon_cd

--- install_desktop.sh
+++ install_desktop.sh
@@ -11,6 +11,8 @@ sudo apt install -y ros-humble-desktop python3-colcon-common-extensions python3-
 sudo apt -y install gazebo
 sudo apt install -y ros-humble-gazebo-*
 
+echo -e "\n## Execute $0 $(date +'%Y/%m/%d %H:%M:%S')"  >> ~/.bashrc
+
 grep "source /opt/ros/humble/setup.bash" ~/.bashrc
 if [ $? = 0 ]; then
   :
@@ -23,4 +25,19 @@ if [ $? = 0 ]; then
   :
 else
   echo "export ROS_DOMAIN_ID=1" >> ~/.bashrc
+fi
+
+grep "source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash" ~/.bashrc
+if [ $? = 0 ]; then
+  :
+else
+  echo "source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash" >> ~/.bashrc
+fi
+
+grep "export _colcon_cd_root=" ~/.bashrc
+if [ $? = 0 ]; then
+  :
+else
+  echo "source /usr/share/colcon_cd/function/colcon_cd.sh" >> ~/.bashrc
+  echo "export _colcon_cd_root=~" >> ~/.bashrc
 fi
